8fd29de72f625ac58133636b2b76582ab0a42464
Vaibhav Tupe <141541491+vaibhav0513@users.noreply.github.com>
2024-08-23 17:42:09 +0530

    add portfolio share icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,6 +118,24 @@ for (let i = 0; i < formInputs.length; i++) {
   });
 }
 
+//portfolio modal 
+function openModal() {
+  document.getElementById("techstackModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("techstackModal").style.display = "none";
+}
+
+// Close the modal when clicking outside of it
+window.onclick = function(event) {
+  const modal = document.getElementById("techstackModal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
